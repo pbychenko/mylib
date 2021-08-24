98351adea989a23be6a01801a6279b8683642f39
pavelv <pavelv87@gmail.com>
2021-08-24 16:02:58 +0600

    fix changes

--- pages/ssg.js
+++ pages/ssg.js
@@ -1,5 +1,10 @@
 import Page from '../components/Page'
 
+const t = {
+  counter: 122,
+  todo: [{id:1, title: 'dd'}, {id:1, title: 'd2'}]
+}
+
 export default function SSG() {
   return <Page title="Index Page" linkTo="/other" />
 }
@@ -7,5 +12,5 @@ export default function SSG() {
 // If you build and start the app, the date returned here will have the same
 // value for all requests, as this method gets executed at build time.
 export function getStaticProps() {
-  return { props: { initialState: { counter: 11 } } }
+  return { props: { initialState: { ...t } } }
 }
